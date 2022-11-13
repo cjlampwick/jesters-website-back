611e8be9b01ee9b1a7aae305621a0428a0ad321a
= <=>
2022-11-13 16:33:01 -0300

    vercel changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
-const { v4 } = require('uuid');
 
 const jwt = require("jsonwebtoken");
 const auth = require("./auth");
@@ -36,18 +35,6 @@ dbConnect();
 
 app.use(express.json());
 app.use(cors());
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
 
 app.post("/mp_ipn", async (req, res) =>{
   console.log("ASD");
@@ -437,5 +424,3 @@ app.get("/news", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-module.exports = app;
